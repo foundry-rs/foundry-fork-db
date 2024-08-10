c5815522aee883b07d12d4e919a1df272690fa76
nk_ysg <nk_ysg@163.com>
2024-08-10 10:47:49 +0800

    fix fmt

--- src/backend.rs
+++ src/backend.rs
@@ -541,11 +541,11 @@ impl BlockingMode {
 
 /// A cloneable backend type that shares access to the backend data with all its clones.
 ///
-/// This backend type is connected to the `BackendHandler` via a    mpsc unbounded channel. The `BackendHandler`
-/// is spawned on a tokio task and listens for incoming commands on the receiver half of the
-/// channel. A `SharedBackend` holds a sender for that channel, which is `Clone`, so there can be
-/// multiple `SharedBackend`s communicating with the same `BackendHandler`, hence this `Backend`
-/// type is thread safe.
+/// This backend type is connected to the `BackendHandler` via a mpsc unbounded channel. The
+/// `BackendHandler` is spawned on a tokio task and listens for incoming commands on the receiver
+/// half of the channel. A `SharedBackend` holds a sender for that channel, which is `Clone`, so
+/// there can be multiple `SharedBackend`s communicating with the same `BackendHandler`, hence this
+/// `Backend` type is thread safe.
 ///
 /// All `Backend` trait functions are delegated as a `BackendRequest` via the channel to the
 /// `BackendHandler`. All `BackendRequest` variants include a sender half of an additional channel
