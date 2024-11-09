4ea5f759126a4141063ee42e29285b6e162b5779
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2024-11-09 21:46:11 +0530

    fix: accept generic header in meta builder (#30)
    * fix: `with_block` with generic header
    
    * fix: accept generic header in builder

--- src/backend.rs
+++ src/backend.rs
@@ -862,6 +862,19 @@ mod tests {
     const ENDPOINT: Option<&str> = option_env!("ETH_RPC_URL");
 
     #[tokio::test(flavor = "multi_thread")]
+    async fn test_builder() {
+        let Some(endpoint) = ENDPOINT else { return };
+        let provider = get_http_provider(endpoint);
+
+        let any_rpc_block = provider
+            .get_block(BlockId::latest(), alloy_rpc_types::BlockTransactionsKind::Hashes)
+            .await
+            .unwrap()
+            .unwrap();
+        let _meta = BlockchainDbMeta::default().with_block(&any_rpc_block.inner);
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
     async fn shared_backend() {
         let Some(endpoint) = ENDPOINT else { return };
 
